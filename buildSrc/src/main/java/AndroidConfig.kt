a63418e9f44ecc083a96a0e30a2fed76eb92a7c7
DomeOfHeaven <domeofheaven2017@outlook.com>
2020-08-11 21:20:21 +0800

    【master-init】初始化项目

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,12 @@
+
+object AndroidConfig {
+
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.0"
+
+    const val applicationId = "com.wx.exercise"
+    const val minSdkVersion = 22
+    const val targetSdkVersion = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,31 @@
+
+
+object Version {
+
+    const val kt_stdlib = "1.3.72"
+    const val ktx_core = "1.3.1"
+    const val appcompat = "1.1.0"
+
+    const val junit = "4.12"
+    const val junit_ext = "1.1.1"
+    const val espresso = "3.2.0"
+
+    const val constrantLayout = "1.1.3"
+
+
+}
+
+object AndroidDeps {
+    const val kt_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Version.kt_stdlib}"
+    const val ktx_core = "androidx.core:core-ktx:${Version.ktx_core}"
+    const val junit = "junit:junit:${Version.junit}"
+    const val junit_ext = "androidx.test.ext:junit:${Version.junit_ext}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Version.espresso}"
+
+    const val appcompat = "androidx.appcompat:appcompat:${Version.appcompat}"
+    const val constrantLayout = "androidx.constraintlayout:constraintlayout:${Version.constrantLayout}"
+}
+
+object ThirdDeps {
+
+}
